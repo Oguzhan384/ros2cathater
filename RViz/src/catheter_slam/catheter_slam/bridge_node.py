ea528fb1062175761b7aff210151c7f42e381fef
oguzhan <oguzhan.yilmaz@ug.bilkent.edu.tr>
2026-08-26 15:18:28 +0300

    Slamde iyileştirmeler

--- RViz/src/catheter_slam/catheter_slam/bridge_node.py
+++ RViz/src/catheter_slam/catheter_slam/bridge_node.py
@@ -30,9 +30,9 @@ class CatheterBridge(Node):
         except Exception as e:
             self.get_logger().error(f"Port hatasi: {e}"); raise e
 
-        # --- PARAMETRELER ---
-        self.v_min, self.v_max = 0.8, 1.3
-        self.r_min, self.r_max = 0.015, 0.025
+        # --- PARAMETRELER (Metre cinsinden)---
+        self.v_min, self.v_max = 1.4, 2.05
+        self.r_min, self.r_max = 0.01, 0.017
         
         # Hareket mesafesini 25 kat büyüten çarpan
         self.visual_x_scale = 25.0 
@@ -41,19 +41,21 @@ class CatheterBridge(Node):
         
         self.colors = [(1,0,0),(1,0.4,0),(1,1,0),(0.5,1,0),(0,1,0),(0,1,0.5),(0,1,1),(0,0.5,1),(0,0,1),(0.4,0,1),(0.7,0,1),(1,0,1),(1,0,0.5),(0.6,0.6,0.6),(1,1,1)]
         
-        WHEEL_RADIUS_M = 0.015
+        WHEEL_RADIUS_M = 0.015 #(Metre cinsinden)
         PPR = 800.0
         self.P_METRE = (2 * math.pi * WHEEL_RADIUS_M) / PPR
 
         # --- KONTROL BAYRAKLARI ---
-        self.noise_enabled = False
-        self.processing_enabled = True
+        self.noise_enabled = True
 
+        # --- GÜRÜLTÜ ZAMANLAMASI ---
+        self.noise_cycle_period = 2.0  # Toplam döngü süresi (saniye)
+        self.noise_on_duration = 1.0   # Gürültünün açık kalacağı süre (saniye)
         # --- FİLTRE PARAMETRELERİ ---
         self.v_filtered = None
         self.v_alpha = 0.15 
 
-        self.cmd_sub = self.create_subscription(String, 'catheter_cmd', self._cmd_callback, 10)
+        #self.cmd_sub = self.create_subscription(String, 'catheter_cmd', self._cmd_callback, 10) (Tuş ile kontrol)
 
         self.is_tty = sys.stdin.isatty()
         if self.is_tty:
@@ -86,25 +88,27 @@ class CatheterBridge(Node):
         self.create_timer(0.1, self._publish_legend_dynamic)
         self.create_timer(0.4, self._republish_all_markers)
 
-    def _cmd_callback(self, msg):
-        command = msg.data.lower()
-        if command == 'n': self.noise_enabled = not self.noise_enabled
-        elif command == 'p': self.processing_enabled = not self.processing_enabled
+    #-- Tuş ile kontrol --
+    #def _cmd_callback(self, msg):
+    #    command = msg.data.lower()
+    #    if command == 'n': self.noise_enabled = not self.noise_enabled
 
-    def _get_key(self):
-        if not self.is_tty: return None
-        tty.setraw(sys.stdin.fileno())
-        key = sys.stdin.read(1)
-        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.settings)
-        return key
+    #def _get_key(self):
+    #    if not self.is_tty: return None
+    #    tty.setraw(sys.stdin.fileno())
+    #    key = sys.stdin.read(1)
+    #    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.settings)
+    #    return key
 
     def run(self):
         while rclpy.ok():
             try:
-                if self.is_tty and select.select([sys.stdin], [], [], 0)[0]:
-                    key = self._get_key()
-                    if key == 'n': self.noise_enabled = not self.noise_enabled
-                    elif key == 'p': self.processing_enabled = not self.processing_enabled
+
+                #-- Tuş ile kontrol --
+                #if self.is_tty and select.select([sys.stdin], [], [], 0)[0]:
+                #    key = self._get_key()
+                #    if key == 'n': self.noise_enabled = not self.noise_enabled
+
 
                 waiting = self.ser.in_waiting
                 if waiting < 6:
@@ -115,6 +119,12 @@ class CatheterBridge(Node):
                 
                 now_ros = self.get_clock().now()
                 now_msg = now_ros.to_msg()
+                now_sec = now_ros.nanoseconds / 1e9
+
+                # Noise için zaman döngüsü hesaplama
+                time_in_cycle = now_sec % self.noise_cycle_period
+
+                is_noise_time = time_in_cycle < self.noise_on_duration #Noise süresinin açık durması
 
                 for adc_raw, enc_raw in struct.iter_unpack('>Hi', data):
                     if self.prev_enc is None:
@@ -122,10 +132,11 @@ class CatheterBridge(Node):
                     
                     delta = enc_raw - self.prev_enc
                     
-                    if self.processing_enabled:
-                        # Gürültü ve mesafe visual_x_scale ile çarpıldı
-                        noise = abs(np.random.normal(0, 1.0)) if self.noise_enabled else 0.0
-                        self.odom_x += (delta + noise) * self.P_METRE * self.visual_x_scale
+                    current_noise = 0.0
+                    if self.noise_enabled and is_noise_time:
+                        current_noise = abs(np.random.normal(0, 0.2))
+
+                    self.odom_x += (delta + current_noise) * self.P_METRE * self.visual_x_scale
                     
                     self.prev_enc = enc_raw
 
@@ -179,11 +190,10 @@ class CatheterBridge(Node):
                             seg[3] = current_r
 
                     # Yeni Segment Ekleme
-                    if self.processing_enabled:
-                        last_end_x = self.vessel_segments[-1][2] if self.vessel_segments else 0.0
-                        if self.current_lx > last_end_x + 0.001:
-                            self.marker_id_counter += 1
-                            self.vessel_segments.append([self.marker_id_counter, last_end_x, self.current_lx, current_r, False])
+                    last_end_x = self.vessel_segments[-1][2] if self.vessel_segments else 0.0
+                    if self.current_lx > last_end_x + 0.001:
+                        self.marker_id_counter += 1
+                        self.vessel_segments.append([self.marker_id_counter, last_end_x, self.current_lx, current_r, False])
                 
                 rclpy.spin_once(self, timeout_sec=0)
             except Exception: pass
@@ -216,17 +226,36 @@ class CatheterBridge(Node):
         self.marker_pub.publish(ma)
 
     def _publish_legend_static(self):
-        ma = MarkerArray(); now = self.get_clock().now().to_msg(); step = (self.r_max - self.r_min) / 14.0
-        for i in range(15):
-            val, col = self.r_min + (i * step), self.colors[i]
-            m_c = Marker()
-            m_c.header.stamp, m_c.header.frame_id = now, "map"
-            m_c.ns, m_c.id, m_c.type = "legend", i, Marker.CUBE
-            m_c.pose.position.x, m_c.pose.position.y, m_c.pose.position.z = -0.1, -0.3, 0.1 + (i*0.02)
-            m_c.scale.x = m_c.scale.y = m_c.scale.z = 0.015
-            m_c.color.r, m_c.color.g, m_c.color.b, m_c.color.a = float(col[0]), float(col[1]), float(col[2]), 1.0
-            ma.markers.append(m_c)
-        self.marker_pub.publish(ma)
+            ma = MarkerArray()
+            now = self.get_clock().now().to_msg()
+            step = (self.r_max - self.r_min) / 14.0
+            
+            for i in range(15):
+                val = self.r_min + (i * step)
+                col = self.colors[i]
+                
+                # 1. Renkli Kutu (Küp)
+                m_c = Marker()
+                m_c.header.stamp, m_c.header.frame_id = now, "map"
+                m_c.ns, m_c.id, m_c.type = "legend_cubes", i, Marker.CUBE
+                m_c.pose.position.x, m_c.pose.position.y, m_c.pose.position.z = -0.1, -0.3, 0.1 + (i*0.02)
+                m_c.scale.x = m_c.scale.y = m_c.scale.z = 0.015
+                m_c.color.r, m_c.color.g, m_c.color.b, m_c.color.a = float(col[0]), float(col[1]), float(col[2]), 1.0
+                ma.markers.append(m_c)
+
+                # 2. Değer Metni (Yazı)
+                m_t = Marker()
+                m_t.header.stamp, m_t.header.frame_id = now, "map"
+                m_t.ns, m_t.id, m_t.type = "legend_text", i, Marker.TEXT_VIEW_FACING
+                m_t.pose.position.x, m_t.pose.position.y, m_t.pose.position.z = -0.1, -0.35, 0.1 + (i*0.02)
+                m_t.scale.z = 0.015 # Yazı boyutu
+                m_t.color.r, m_t.color.g, m_t.color.b, m_t.color.a = 1.0, 1.0, 1.0, 1.0 # Beyaz yazı
+                
+                # Metreyi mm'ye çevirip yazdırıyoruz (Örn: 0.01 -> 10.0 mm)
+                m_t.text = f"{val * 1000:.1f} mm"
+                ma.markers.append(m_t)
+                
+            self.marker_pub.publish(ma)
 
     def _publish_legend_dynamic(self):
             ma = MarkerArray()
